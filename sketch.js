51870401841189e4e47570c408c2d6ffaffa9630
Michael Smith <msmith@bcimedia.com>
2017-01-12 09:27:54 -0500

    Initial snake project

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var snake;
+var pixel_size = 20;
+var shots = [];
+var movement = [];
+
+function setup(){
+  createCanvas(600, 600);
+  frameRate(10);
+  snake = new Snake();
+  setJelloShots(5);
+
+}
+
+function draw(){
+  background(50, 50, 100);
+
+  snake.update();
+  snake.show();
+  snake.checkDeath();
+
+  fill(0, 255, 0, 100);
+  for(var i=0;i<shots.length;i++){
+    rect(shots[i].x, shots[i].y, pixel_size, pixel_size);
+    if(snake.eat(shots[i])){
+      snake.tail.push(createVector(snake.x, snake.y));
+      shots.splice(i, 1);
+      setJelloShots(1);
+    }
+  }
+}
+
+function setJelloShots(num){
+  var cols = floor(width / pixel_size);
+  var rows = floor(height / pixel_size);
+  for(var i=0;i<num;i++){
+    var location = createVector(floor(random(cols)), floor(random(rows))).mult(pixel_size);
+    while(snake_intersect(location)){
+      console.log("interesct caught at:", location);
+      location = createVector(floor(random(cols)), floor(random(rows))).mult(pixel_size);
+    }
+    shots.push(location);
+  }
+}
+
+function snake_intersect(location){
+  var intersect = false;
+  if(location.x == snake.pos.x && location.y == snake.pos.y){
+    intersect = true;
+  }else{
+    for(var i=0;i<snake.tail.length;i++){
+      if(location.x == snake.tail[i].x && location.y == snake.tail[i].y){
+        intersect = true;
+        break;
+      }
+    }
+    for(var i=0;i<shots.length;i++){
+      if(location.x == shots[i].x && location.y == shots[i].y){
+        intersect = true;
+        break;
+      }
+    }
+  }
+  return intersect;
+}
+
+function keyPressed(){
+  if(keyCode === DOWN_ARROW){
+    movement.push([0, 1]);
+  }else if(keyCode === UP_ARROW){
+    movement.push([0, -1]);
+  }else if(keyCode === LEFT_ARROW){
+    movement.push([-1, 0]);
+  }else if(keyCode === RIGHT_ARROW){
+    movement.push([1, 0]);
+  }
+}
+
---
+++ snake.js
@@ -0,0 +1,59 @@
+function Snake(){
+  this.show = function(){
+    fill(255);
+    //draw the snake tail
+    for(var i=0;i<this.tail.length;i++){
+      rect(this.tail[i].x, this.tail[i].y, pixel_size, pixel_size);
+    }
+
+    //draw the snake head
+    rect(this.pos.x, this.pos.y, pixel_size, pixel_size)
+  }
+
+  this.update = function(){
+    //move snake's position into tail and pop off the end
+    if(movement.length){
+      if(snake.speed.x != movement[0][0]*-1 && snake.speed.y != movement[0][1]*-1){
+        snake.dir(movement[0][0], movement[0][1]);
+      }
+      movement.splice(0, 1);
+    }
+
+    this.tail.unshift(createVector(this.pos.x, this.pos.y));
+    this.tail.pop();
+    //move the snake
+    this.pos.x += this.speed.x * pixel_size;
+    this.pos.y += this.speed.y * pixel_size;
+  }
+
+  this.dir = function(x, y){
+    this.speed.x = x;
+    this.speed.y = y;
+  }
+
+  this.checkDeath = function(){
+    if(this.pos.x > width || this.pos.y > height || this.pos.x < 0 || this.pos.y < 0){
+      this.reset();
+      setJelloShots(5);
+    }
+    for(var i=0;i<this.tail.length;i++){
+      if(this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y){
+        this.reset();
+        setJelloShots(5);
+      }
+    }
+  }
+
+  this.eat = function(pos){
+    return this.pos.x == pos.x && this.pos.y == pos.y;
+  }
+
+  this.reset = function(){
+    shots = [];
+    this.tail = [];
+    this.pos = createVector(0, 0);
+    this.speed = createVector(1, 0);
+  }
+
+  this.reset();
+}
