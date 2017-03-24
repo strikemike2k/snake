df96e2d15616d82dca23756971a68f9730d86060
ksaliya <saliyakumara@yahoo.com>
2017-03-24 15:31:04 -0400

    Code cosmetics
    removed some commented lines

--- snake.js
+++ snake.js
@@ -34,14 +34,10 @@ function Snake(){
   this.checkDeath = function(){
     if(this.pos.x >= width || this.pos.y >= height || this.pos.x < 0 || this.pos.y < 0){
     	gameState = 'end';
-      // this.reset();
-      // setJelloShots(5);
     }
     for(var i=0;i<this.tail.length;i++){
       if(this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y){
       	gameState = 'end';
-        // this.reset();
-        // setJelloShots(5);
       }
     }
   }
